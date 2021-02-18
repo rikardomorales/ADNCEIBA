6093c215f185af514c47fcd511888087bf9e9e84
Ricardo Alejandro Morales Penilla - Ceiba Software <ricardo.morales@ceiba.com.co>
2021-02-17 19:27:23 -0500

    last

--- microservicio/dominio/src/test/java/com/ceiba/pago/servicio/ServicioCrearPagoTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/pago/servicio/ServicioCrearPagoTest.java
@@ -13,16 +13,17 @@ public class ServicioCrearPagoTest {
     private static final Long ID_PAGO =  99999L;
 
     @Test
-    public void crearPagoTest() {
+    public void crearPagoTest() throws Exception {
         // arrange
         PagoTestDataBuilder PagoTestDataBuilder = new PagoTestDataBuilder().conIdPago(ID_PAGO);
 
         RepositorioPago repositorioPago = Mockito.mock(RepositorioPago.class);
-        ServicioActualizarPago servicioActualizarPago = new ServicioActualizarPago(repositorioPago);
+        ServicioCrearPago servicioCrearPago = new ServicioCrearPago(repositorioPago);
 
         //act
         Pago pago = new PagoTestDataBuilder().build();
         Mockito.when(repositorioPago.crear(pago)).thenReturn(ID_PAGO);
+        servicioCrearPago.ejecutar(pago);
 
         //assert
         assertEquals(pago.getIdPago(), ID_PAGO);
