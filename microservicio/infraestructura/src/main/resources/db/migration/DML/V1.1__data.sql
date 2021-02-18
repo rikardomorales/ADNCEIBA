1867e8649de17d4e310e7ec7751a08f31b93b25d
Ricardo Alejandro Morales Penilla - Ceiba Software <ricardo.morales@ceiba.com.co>
2021-02-17 20:36:05 -0500

    last

--- microservicio/infraestructura/src/main/resources/db/migration/DML/V1.1__data.sql
+++ microservicio/infraestructura/src/main/resources/db/migration/DML/V1.1__data.sql
@@ -1,8 +1,6 @@
-insert into pagos
-(id_pago,documento_Identificacion_Deudor,codigo_Factura,valor_Adeudado,
-valor_Pagado,fecha_Vencimiento_Pago,fecha_Pago)
+insert into pagos (id_pago,documento_Identificacion_Deudor,codigo_Factura,
+valor_Adeudado,valor_Pagado,fecha_Vencimiento_Pago,fecha_Pago)
 values(1,'123456789','FV-9898',1000000,0,'2020-03-30','');
-insert into pagos
-(id_pago,documento_Identificacion_Deudor,codigo_Factura,valor_Adeudado,
-valor_Pagado,fecha_Vencimiento_Pago,fecha_Pago)
+insert into pagos (id_pago,documento_Identificacion_Deudor,codigo_Factura,
+valor_Adeudado,valor_Pagado,fecha_Vencimiento_Pago,fecha_Pago)
 values(2,'123456789','FV-9899',500000,0,'2020-03-30','');
